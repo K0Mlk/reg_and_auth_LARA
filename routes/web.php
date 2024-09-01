56bc89144c6ec846c84d9ae9d65f1b4eb3913dee
K0Mlk <k0mlk99c0de@gmail.com>
2024-09-02 00:45:52 +0500

    - Added vue - Adding routing for registration

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 
 Route::view('/', 'welcome')->name('welcome');
-Route::view('/dashboard', 'dashboard')->name('dashboard');
-Route::view('/register', 'auth.register')->name('register');
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::view('/dashboard', 'dashboard')->middleware('auth')->name('dashboard');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
